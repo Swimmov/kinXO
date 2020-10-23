04d1e8c25987fc4b9fbb725b0c20107a7b1645f0
Swimmov <dimtest191@gmail.com>
2020-10-23 12:50:46 -0500

    add switch method to .java file

--- src/main/java/KinGameXO.java
+++ src/main/java/KinGameXO.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class KinGameXO {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         char[][] gameBoard = {
                 {' ', '|', ' ', '|', ' '},
@@ -7,7 +11,12 @@ public class KinGameXO {
                 {' ', '|', ' ', '|', ' '},
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}, };
-       printGameBoard(gameBoard);
+
+       BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter your placement (1-9):");
+       int pos  = Integer.parseInt(bfr.readLine());
+        printGameBoard(gameBoard);
+
     }
 
     public static void printGameBoard(char[][] gameBoard) {
@@ -18,6 +27,41 @@ public class KinGameXO {
             System.out.println();
 
         }
+    }
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+            default:
+                break;
+
+        }
 
     }
+
 }
