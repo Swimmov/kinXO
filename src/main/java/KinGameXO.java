9ec0d551e2be0e7d05a1b5915b3e3fc8102e0574
Swimmov <dimtest191@gmail.com>
2020-10-23 09:53:11 -0500

    some code added to .java file

--- src/main/java/KinGameXO.java
+++ src/main/java/KinGameXO.java
@@ -1,7 +1,23 @@
 public class KinGameXO {
     public static void main(String[] args) {
-String str = "Hello!";
-        System.out.println(str);
-        int i = 89;
+
+        char[][] gameBoard = {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}, };
+       printGameBoard(gameBoard);
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char [] raw : gameBoard) {
+            for (char c : raw) {
+                System.out.print(c);
+            }
+            System.out.println();
+
+        }
+
     }
 }
